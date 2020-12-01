6547854a9f08c53e2339f5abf6cc2cd8e27ad15d
Hemant <Hemant1989>
2020-12-01 15:22:31 +0530

    Updated file

--- src/test/java/basicjavaprograms/WelcomeTest.java
+++ src/test/java/basicjavaprograms/WelcomeTest.java
@@ -4,7 +4,7 @@ public class WelcomeTest {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in HugeLearning.com");
-		System.out.println("test");
+		System.out.println("test1");
 	}
 
 }
