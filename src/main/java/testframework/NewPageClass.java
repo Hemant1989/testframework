8af01c7fda90c9dd828754efeedc8a5a66f746f4
Hemant Negi <hemantnegi89@gmail.com>
2023-09-19 11:42:30 +0530

    Updated NewPageClass.java

--- src/main/java/testframework/NewPageClass.java
+++ src/main/java/testframework/NewPageClass.java
@@ -3,6 +3,6 @@ package testframework;
 public class NewPageClass {
 
     public static void main(String[] args) {
-        System.out.print("In NewPageClass.java");
+        System.out.print("Updated NewPageClass.java");
     }
 }
